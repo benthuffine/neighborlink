343ff43ec4c3c1f17711f5493754dfdfea29384e
Ben Huffine <benthuffine@gmail.com>
2011-11-16 22:45:52 -0500

    bth: content

---
+++ apps/content/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='ext/heroshots')
+    description = models.CharField(max_length=255)
+    url = models.CharField(max_length=255,blank=True)
+    sort_order = models.PositiveIntegerField(null=True, blank=True)
+
+class Page(models.Model):
+	heroshots = models.ManyToManyField(Image, null=True)
+	content = models.TextField(null=True, blank=True)
---
+++ apps/content/views.py
@@ -0,0 +1,15 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.http import HttpResponseRedirect
+
+from neighborlink.apps.content.models import *
+
+def home(request):
+	contentpage = get_object_or_404(Page, slug__exact='home')
+
+	context = RequestContext(request, {
+        'contentpage': contentpage,
+    })
+    
+    return render_to_response('home.html', context)

23a07efaea98f20fcd3c7e2b3b6f3327e9d4eed7
Ben Huffine <benthuffine@gmail.com>
2011-11-17 21:56:15 -0500

    bth: admin

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from neighborlink.apps.content.models import *
 
+class HeroshotInline(admin.TabularInline):
+    model = Heroshot
+    extra = 3
+    
+class PageAdmin(admin.ModelAdmin):
+	inlines = [
+		HeroshotInline,
+	]
 admin.site.register(Page)

5054da9b6450dcabc30ca55facbe810a56119046
Ben Huffine <benthuffine@gmail.com>
2011-11-21 13:19:26 -0500

    bth: st elmo

--- host_settings/ben.py
+++ host_settings/ben.py
@@ -12,3 +12,36 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.flatpages',
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'neighborlink.apps.account',
+    'neighborlink.apps.content',
+    'neighborlink.apps.entity',
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}

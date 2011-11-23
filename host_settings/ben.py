665e47599296069a130b60d246c7e1015a850779
Ben Huffine <benthuffine@gmail.com>
2011-11-23 14:06:04 -0500

    bth: stuff

--- host_settings/ben.py
+++ host_settings/ben.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.flatpages',
+    'django.contrib.comments',
     'grappelli',
     'django.contrib.admin',
     'django.contrib.admindocs',

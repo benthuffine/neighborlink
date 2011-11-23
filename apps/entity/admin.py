e6a3bc765c98f330caf732521676cebad13ddd3e
Ben Huffine <benthuffine@gmail.com>
2011-11-23 10:24:56 -0500

    bth: stuff

--- apps/entity/admin.py
+++ apps/entity/admin.py
@@ -4,9 +4,20 @@ from neighborlink.apps.entity.models import *
 class EntityAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
-admin.site.register(Business, EntityAdmin)
+
 admin.site.register(Church, EntityAdmin)
 admin.site.register(Organization, EntityAdmin)
 admin.site.register(BusinessType)
 admin.site.register(Denomination)
 admin.site.register(Offer)
+
+class OfferInline(admin.TabularInline):
+    model = Offer
+    extra = 3
+
+class BusinessAdmin(EntityAdmin):
+    inlines = [
+        OfferInline,
+    ]    
+
+admin.site.register(Business, BusinessAdmin)    

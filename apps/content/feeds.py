0bc6a5f3b633b5286e33911b1f63f1e97123bd15
Ben Huffine <benthuffine@gmail.com>
2011-12-22 16:57:15 -0500

    bth: fixing news feed

--- apps/content/feeds.py
+++ apps/content/feeds.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.syndication.views import Feed    
 from neighborlink.apps.content.models import *
 
-from datetime import datetime
+import datetime
 
 class LatestNewsEventsFeed(Feed):
     title = "%s News and Events" % settings.SITE_NAME
@@ -10,7 +10,7 @@ class LatestNewsEventsFeed(Feed):
     description = "Latest news and events for %s" % settings.SITE_NAME
 
     def items(self):
-        return NewsEvent.objects.filter(start_date__lte=datetime.now(), end_date__gte=datetime.now(), approved=True).order_by('-event_start_date')[:5]
+        return NewsEvent.objects.filter(start_date__lte=datetime.datetime.now(), end_date__gte=datetime.datetime.now(), approved=True).order_by('-event_start_date')[:5]
 
     def item_title(self, item):
         return item.title
@@ -19,7 +19,7 @@ class LatestNewsEventsFeed(Feed):
         return item.content
 
     def item_pubdate(self, item):
-        return item.insert_date
+        return datetime.datetime.combine(item.insert_date, datetime.time())
 
 class LatestAboutPageFeed(Feed):
     title = "%s Articles" % settings.SITE_NAME
@@ -36,4 +36,4 @@ class LatestAboutPageFeed(Feed):
         return item.content
 
     def item_pubdate(self, item):
-        return item.insert_date
+        return datetime.datetime.combine(item.insert_date, datetime.time())
